0b65986edb189613db274f9870c0b9cf8c33183b
Guilherme Rodrigues <guilodron7@gmail.com>
2020-09-14 21:29:35 -0300

    Finaliza tela de introdução

--- src/pages/IntroPage/index.js
+++ src/pages/IntroPage/index.js
@@ -1,11 +1,51 @@
 import React from 'react'
-import { View, Text, SafeAreaView } from 'react-native'
+import LogoMeau from '../../../assets/marca-meau.png';
+import {  Text  } from 'react-native'
+import { useFonts, Courgette_400Regular } from '@expo-google-fonts/courgette'
+import { Container, Greeting, AboutText, Button, ButtonText, LoginLink, LoginText, Logo } from './styles'
+import { AppLoading } from 'expo'
 
 const IntroPage = () => {
+
+  let [fontsLoaded] = useFonts({
+    Courgette_400Regular,
+  });
+
+  if( !fontsLoaded ) {
+    return <AppLoading/>
+  }
+
   return (
-    <View>
-      <Text>Intro Page</Text>
-    </View>
+    <Container>
+      <Greeting>Olá!</Greeting>
+      <AboutText>
+        Bem vindo ao Meau!{`\n`}
+        Aqui você pode adotar, doar e ajudar{`\n`}
+        cães e gatos com facilidade.{`\n`}
+        Qual o seu interesse?
+      </AboutText>
+      <Button>
+        <ButtonText>
+          ADOTAR
+        </ButtonText>
+      </Button>
+      <Button>
+        <ButtonText>
+          AJUDAR
+        </ButtonText>
+      </Button>
+      <Button>
+        <ButtonText>
+          CADASTRAR ANIMAL
+        </ButtonText>
+      </Button>
+      <LoginLink>
+        <LoginText>login</LoginText>
+      </LoginLink>
+      <Logo source={LogoMeau}>
+
+      </Logo>
+    </Container>
   )
 }
 
