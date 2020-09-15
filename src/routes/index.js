d576fd73ad35c19e48c1be9730b8d9facc300e87
Guilherme Rodrigues <guilodron7@gmail.com>
2020-09-15 15:14:27 -0300

    Implements Animal Registration page

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,7 @@ import LoginPage from '../pages/LoginPage';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import AnimalRegistrationPage from '../pages/AnimalRegistrationPage';
 
 const Stack = createStackNavigator();
 
@@ -17,6 +18,16 @@ const Routes = () => {
           title: 'Login',
           headerStyle: {
             backgroundColor: '#cfe9e5',
+          },
+          headerTintColor: '#434343',
+          headerTitleStyle: {
+            fontSize: 20
+          },
+        }} />
+        <Stack.Screen name="AnimalRegistration" component={AnimalRegistrationPage} options={{
+          title: 'Cadastro do Animal',
+          headerStyle: {
+            backgroundColor: '#ffd358',
           },
           headerTintColor: '#434343',
           headerTitleStyle: {
